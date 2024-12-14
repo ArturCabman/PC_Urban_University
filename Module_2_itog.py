be4d75eae092633d6ada5b507cfc7e5d443e882f
Артур <katokataka@gmail.com>
2024-12-14 22:23:10 +0700

    Допзадание

---
+++ Module_2_itog.py
@@ -0,0 +1,13 @@
+def open_the_gate(number):
+    password = list()
+    comb_ = number + 1 // 2
+    for i in range(1, comb_):
+        j = i + 1
+        while i + j <= number:
+            if number % (i + j) == 0:
+                pass_numbers = str(i) + str(j)
+                password.append(pass_numbers)
+            j += 1
+    return password
+input_number = int(input('Input number: '))
+print(open_the_gate(input_number))
