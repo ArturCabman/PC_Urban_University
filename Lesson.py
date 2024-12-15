b89775c83b259635622c6b685ecbb944d6a04195
Артур <katokataka@gmail.com>
2024-12-15 15:08:26 +0700

    Крестики нолики украсил

--- Lesson.py
+++ Lesson.py
@@ -36,21 +36,23 @@ def check_winner():
 def draw_area():
     for i in area:
         print(*i)
-area = [['*', '*', '*'], ['*', '*', '*'], ['*', '*', '*']]
+area = [[' *', ' *', ' *'], [' *', ' *', ' *'], [' *', ' *', ' *']]
 print('Добро пожаловать в крестики-нолики!')
 print('-----------------------------------')
+print()
 draw_area()
+print()
 for turn in range(1, 10):
     print(f'Ход: {turn}')
     if turn % 2 == 0:
-        turn_char = '0'
+        turn_char = ' 0'
         print('Ходят нолики')
     else:
-        turn_char = 'X'
+        turn_char = ' X'
         print('Ходят крестики')
     row = int(input('Введите номер строки ')) - 1
     column = int(input('Введите номер столбца ')) - 1
-    if area[row][column] == '*':
+    if area[row][column] == ' *':
         area[row][column] = turn_char
     else:
         print('Ячейка занята, пропуск хода!')
