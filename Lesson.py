e1a304755967d01601543a765fe5c4fdc7f04cdd
Артур <katokataka@gmail.com>
2024-12-15 14:48:20 +0700

    Крестики нолики проверка

--- Lesson.py
+++ Lesson.py
@@ -1,3 +1,37 @@
+def check_winner():
+    if area[0][0] == 'X' and area[0][1] == 'X' and area[0][2] == 'X':
+        return 'X'
+    if area[1][0] == 'X' and area[1][1] == 'X' and area[1][2] == 'X':
+        return 'X'
+    if area[2][0] == 'X' and area[2][1] == 'X' and area[2][2] == 'X':
+        return 'X'
+    if area[0][0] == 'X' and area[1][0] == 'X' and area[2][0] == 'X':
+        return 'X'
+    if area[0][1] == 'X' and area[1][1] == 'X' and area[2][1] == 'X':
+        return 'X'
+    if area[0][2] == 'X' and area[1][2] == 'X' and area[2][2] == 'X':
+        return 'X'
+    if area[0][0] == 'X' and area[1][1] == 'X' and area[0][2] == 'X':
+        return 'X'
+    if area[0][2] == 'X' and area[1][1] == 'X' and area[1][2] == 'X':
+        return 'X'
+    if area[0][0] == '0' and area[0][1] == '0' and area[0][2] == '0':
+        return '0'
+    if area[1][0] == '0' and area[1][1] == '0' and area[1][2] == '0':
+        return '0'
+    if area[2][0] == '0' and area[2][1] == '0' and area[2][2] == '0':
+        return '0'
+    if area[0][0] == '0' and area[1][0] == '0' and area[2][0] == '0':
+        return '0'
+    if area[0][1] == '0' and area[1][1] == '0' and area[2][1] == '0':
+        return '0'
+    if area[0][2] == '0' and area[1][2] == '0' and area[2][2] == '0':
+        return '0'
+    if area[0][0] == '0' and area[1][1] == '0' and area[0][2] == '0':
+        return '0'
+    if area[0][2] == '0' and area[1][1] == '0' and area[1][2] == '0':
+        return '0'
+
 def draw_area():
     for i in area:
         print(*i)
