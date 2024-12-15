2d35507a15f1bc7b0fd8d399a59d192a055367ab
Артур <katokataka@gmail.com>
2024-12-15 16:43:13 +0700

    Крестики нолики украсил

--- tic tac.py
+++ tic tac.py
@@ -1,47 +1,46 @@
 def check_winner():
-    if area[0][0] == 'X' and area[0][1] == 'X' and area[0][2] == 'X':
-        return 'X'
     if area[1][0] == 'X' and area[1][1] == 'X' and area[1][2] == 'X':
         return 'X'
     if area[2][0] == 'X' and area[2][1] == 'X' and area[2][2] == 'X':
         return 'X'
-    if area[0][0] == 'X' and area[1][0] == 'X' and area[2][0] == 'X':
+    if area[3][0] == 'X' and area[3][1] == 'X' and area[3][2] == 'X':
         return 'X'
-    if area[0][1] == 'X' and area[1][1] == 'X' and area[2][1] == 'X':
+    if area[1][0] == 'X' and area[2][0] == 'X' and area[3][0] == 'X':
         return 'X'
-    if area[0][2] == 'X' and area[1][2] == 'X' and area[2][2] == 'X':
+    if area[1][1] == 'X' and area[2][1] == 'X' and area[3][1] == 'X':
         return 'X'
-    if area[0][0] == 'X' and area[1][1] == 'X' and area[0][2] == 'X':
+    if area[1][2] == 'X' and area[2][2] == 'X' and area[3][2] == 'X':
         return 'X'
-    if area[0][2] == 'X' and area[1][1] == 'X' and area[1][2] == 'X':
+    if area[1][0] == 'X' and area[2][1] == 'X' and area[1][2] == 'X':
         return 'X'
-    if area[0][0] == '0' and area[0][1] == '0' and area[0][2] == '0':
-        return '0'
+    if area[1][2] == 'X' and area[2][1] == 'X' and area[2][2] == 'X':
+        return 'X'
     if area[1][0] == '0' and area[1][1] == '0' and area[1][2] == '0':
         return '0'
     if area[2][0] == '0' and area[2][1] == '0' and area[2][2] == '0':
         return '0'
-    if area[0][0] == '0' and area[1][0] == '0' and area[2][0] == '0':
+    if area[3][0] == '0' and area[3][1] == '0' and area[3][2] == '0':
         return '0'
-    if area[0][1] == '0' and area[1][1] == '0' and area[2][1] == '0':
+    if area[1][0] == '0' and area[2][0] == '0' and area[3][0] == '0':
         return '0'
-    if area[0][2] == '0' and area[1][2] == '0' and area[2][2] == '0':
+    if area[1][1] == '0' and area[2][1] == '0' and area[3][1] == '0':
         return '0'
-    if area[0][0] == '0' and area[1][1] == '0' and area[0][2] == '0':
+    if area[1][2] == '0' and area[2][2] == '0' and area[3][2] == '0':
         return '0'
-    if area[0][2] == '0' and area[1][1] == '0' and area[1][2] == '0':
+    if area[1][0] == '0' and area[2][1] == '0' and area[1][2] == '0':
+        return '0'
+    if area[1][2] == '0' and area[2][1] == '0' and area[2][2] == '0':
         return '0'
     return '*'
 
 def draw_area():
     for i in area:
         print(*i)
-area = [[' *', ' *', ' *'], [' *', ' *', ' *'], [' *', ' *', ' *']]
+area = [[], [' *', ' *', ' *'], [' *', ' *', ' *'], [' *', ' *', ' *'], []]
 print('Добро пожаловать в крестики-нолики!')
 print('-----------------------------------')
-print()
 draw_area()
-print()
+num_row = [1, 2, 3]
 for turn in range(1, 10):
     print(f'Ход: {turn}')
     if turn % 2 == 0:
@@ -50,8 +49,13 @@ for turn in range(1, 10):
     else:
         turn_char = ' X'
         print('Ходят крестики')
-    row = int(input('Введите номер строки ')) - 1
-    column = int(input('Введите номер столбца ')) - 1
+    row = int(input('Введите номер строки '))
+    if row in num_row:
+        column = int(input('Введите номер столбца ')) - 1
+    else:
+        print('Вы ввели неверное значение, пропуск хода')
+        draw_area()
+        continue
     if area[row][column] == ' *':
         area[row][column] = turn_char
     else:
@@ -59,7 +63,6 @@ for turn in range(1, 10):
         draw_area()
         continue
 
-    draw_area()
     if check_winner() == 'X':
         print('Победа крестиков')
         break
@@ -69,3 +72,5 @@ for turn in range(1, 10):
     if check_winner() == '*' and turn == 9:
         print('Ничья')
         break
+    else:
+        draw_area()
